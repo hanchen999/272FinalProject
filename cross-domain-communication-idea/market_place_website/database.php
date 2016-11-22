01c7c7dc7f572a22f2757b0b4ed99e673402feb1
root <root@ip-172-31-17-188.us-west-2.compute.internal>
2016-11-22 18:23:53 +0000

    hehe

--- cross-domain-communication-idea/market_place_website/database.php
+++ cross-domain-communication-idea/market_place_website/database.php
@@ -1,11 +1,6 @@
 <?php
-<<<<<<< HEAD
-require_once('database_config.php')
-$connect = new mysqli($dbhos:t, $dbuser, $dbpass, $dbname, $portnumber);
-=======
 require_once('database_config.php');
 $connect = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $portnumber);
->>>>>>> 410502de666d1918b0a0de308dbe048fad6d4de1
 if(!$connect)
 {
 		echo 'Failed to connect';
