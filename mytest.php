44c217fac501b3b226154db4cd91fe6d216e5d08
root <root@ip-172-31-17-188.us-west-2.compute.internal>
2016-11-22 18:06:44 +0000

    hehe

--- mytest.php
+++ mytest.php
@@ -1,16 +1,17 @@
 <?php
 
-$dbhost = 'localhost:3036';
+$dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = '';
 $dbname = 'cmpe272FinalProject';
+$portnumber = 3036;
 
-$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $portnumber);
 
 $SQL = "select * from `market_order`";
 		$result = mysqli_query($conn, $SQL);
 		$return_array = array();
-		while($row = mysql_fetch_assoc($result)){
+		while($row = mysqli_fetch_assoc($result)){
 			array_push($return_array,$row);
 		}
 		print_r(json_encode($return_array));
